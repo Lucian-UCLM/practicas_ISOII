de98108bea9c3229fbfe0187299b779dab605408
Lucian <lucianandrei.farcas@alu.uclm.es>
2022-11-01 11:25:42 +0100

    Cambios menores
    Cambios menores en pantallaLogin y pantallaEmpleadosVicerectorado

--- practicas_ISOII/src/main/java/presentacion/PantallaEmpleadosVicerrectorado.java
+++ practicas_ISOII/src/main/java/presentacion/PantallaEmpleadosVicerrectorado.java
@@ -1,20 +1,35 @@
 package presentacion;
 
 import java.awt.EventQueue;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
 import javax.swing.border.EmptyBorder;
+
+import negocio.controllers.GestorPropuestasCursos;
+import negocio.entities.CursoPropio;
+import negocio.entities.EstadoCurso;
+
 import javax.swing.JList;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ListSelectionModel;
+import javax.swing.JTextField;
 
 
 public class PantallaEmpleadosVicerrectorado extends JFrame {
 	private JPanel contentPane;
 	private JTextPane textPaneEstado;
+	private JTextField fecha_propuesto_text;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -32,41 +47,71 @@ public class PantallaEmpleadosVicerrectorado extends JFrame {
 	public PantallaEmpleadosVicerrectorado() {
 		setTitle("Interfaz de login");
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		setBounds(100, 100, 446, 404);
+		setBounds(100, 100, 650, 347);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Cursos");
-		lblNewLabel.setBounds(99, 50, 41, 14);
-		contentPane.add(lblNewLabel);
+		JList curso_list = new JList();
+		curso_list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		curso_list.setBounds(47, 69, 147, 202);
+		contentPane.add(curso_list);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Selecciona un Estado", "Propuesto", "Validado", "Rechazado", "En matriculación", "En impartición", "Terminado"}));
-		comboBox.setToolTipText("-Cursos-\r\n");
-		comboBox.setBounds(47, 282, 147, 22);
-		contentPane.add(comboBox);
+		GestorPropuestasCursos gestor = new GestorPropuestasCursos(); 
+		List<CursoPropio> lista = new ArrayList<>(); 
+		lista = gestor.listarCursos();
+		DefaultListModel model_curso = new DefaultListModel();
+		int i=0;
+		for (Iterator iterator = lista.iterator(); iterator.hasNext();) {
+			model_curso.addElement(lista.get(i).getId());
+			iterator.next();
+			i++;
+		}
 		
-		JList list = new JList();
-		list.setBounds(235, 69, 147, 202);
-		contentPane.add(list);
+		curso_list.setModel(model_curso);
 		
-		JButton btnNewButton = new JButton("Aplicar");
-		btnNewButton.setBounds(71, 315, 89, 23);
-		contentPane.add(btnNewButton);
+		JLabel curso_label = new JLabel("Cursos");
+		curso_label.setBounds(47, 50, 41, 14);
+		contentPane.add(curso_label);
 		
-		JList list_1 = new JList();
-		list_1.setBounds(47, 69, 147, 202);
-		contentPane.add(list_1);
+		JComboBox estado_combobox = new JComboBox();
+		estado_combobox.setModel(new DefaultComboBoxModel(new EstadoCurso[] {EstadoCurso.PROPUESTO, EstadoCurso.VALIDADO, EstadoCurso.PROPUESTA_RECHAZADA}));
+		estado_combobox.setToolTipText("-Cursos-\r\n");
+		estado_combobox.setBounds(204, 192, 103, 22);
+		contentPane.add(estado_combobox);
 		
-		JLabel lblCursosAprobados = new JLabel("Cursos Aprobados \"Validados\"");
-		lblCursosAprobados.setBounds(258, 50, 162, 14);
-		contentPane.add(lblCursosAprobados);
+		JList curso_aprobado_list = new JList();
+		curso_aprobado_list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		curso_aprobado_list.setBounds(360, 69, 147, 202);
+		contentPane.add(curso_aprobado_list);
 		
-		JButton btnNewButton_1 = new JButton("Dar de Alta");
-		btnNewButton_1.setBounds(258, 282, 89, 23);
-		contentPane.add(btnNewButton_1);
+		JButton aplicar_button = new JButton("Aplicar");
+		aplicar_button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		aplicar_button.setBounds(204, 248, 89, 23);
+		contentPane.add(aplicar_button);
+		
+		JLabel curso_aprobados_label = new JLabel("Cursos Aprobados \"Validados\"");
+		curso_aprobados_label.setBounds(360, 50, 187, 14);
+		contentPane.add(curso_aprobados_label);
+		
+		JButton dar_alta_button = new JButton("Dar de Alta");
+		dar_alta_button.setBounds(517, 248, 109, 23);
+		contentPane.add(dar_alta_button);
+		
+		JLabel fecha_propuesto_label = new JLabel("Curso propuesto el:");
+		fecha_propuesto_label.setBounds(204, 69, 146, 22);
+		contentPane.add(fecha_propuesto_label);
+		
+		fecha_propuesto_text = new JTextField();
+		fecha_propuesto_text.setEditable(false);
+		fecha_propuesto_text.setBounds(204, 89, 96, 19);
+		contentPane.add(fecha_propuesto_text);
+		fecha_propuesto_text.setColumns(10);
 	}
 
 	public void evaluarCurso() {
