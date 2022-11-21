91ecfc01b7f6c67f0d776ffa2ddc80cf9409284e
Rubens05 <rubenggjac@hotmail.com>
2022-11-21 18:46:33 +0100

    Commit pantalla jefe gabinete vicerrectorado consultas (sin terminar)

--- practicas_ISOII/src/main/java/presentacion/PantallaJefeGabineteVicerrectorado.java
+++ practicas_ISOII/src/main/java/presentacion/PantallaJefeGabineteVicerrectorado.java
@@ -12,13 +12,25 @@ import javax.swing.JTextField;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListModel;
+
 import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.text.AttributeSet.ColorAttribute;
+
+import negocio.controllers.GestorPropuestasCursos;
+import negocio.entities.CursoPropio;
+
 import javax.swing.border.LineBorder;
+import javax.swing.plaf.synth.ColorType;
+
 import java.awt.Color;
 import javax.swing.JButton;
 import java.awt.Font;
@@ -30,6 +42,7 @@ public class PantallaJefeGabineteVicerrectorado extends JFrame {
 	private JTextField campoFechaInicio;
 	private JTextField campoFechaFin;
 	private JTable table;
+	GestorPropuestasCursos gestor = new GestorPropuestasCursos();
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -47,14 +60,14 @@ public class PantallaJefeGabineteVicerrectorado extends JFrame {
 	public PantallaJefeGabineteVicerrectorado() {
 		setTitle("Interfaz de jefe de gabinete");
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		setBounds(100, 100, 700, 400);
+		setBounds(100, 100, 180, 400);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(192, 192, 192));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JLabel lblNewLabel = new JLabel("Seleccionar Campos");
+		JLabel lblNewLabel = new JLabel("Seleccione Campos");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblNewLabel.setBounds(29, 26, 125, 14);
 		contentPane.add(lblNewLabel);
@@ -106,72 +119,129 @@ public class PantallaJefeGabineteVicerrectorado extends JFrame {
 		JButton botonConsulta = new JButton("Consultar");
 		botonConsulta.setBounds(31, 326, 89, 23);
 		contentPane.add(botonConsulta);
-		
-		JLabel lblNingunCampoSelected = new JLabel("");
-		lblNingunCampoSelected.setBounds(304, 125, 329, 69);
-		contentPane.add(lblNingunCampoSelected);
 		botonConsulta.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				EventQueue.invokeLater(new Runnable() {
 					public void run() {
-						int i = 0;
-						
+						int numColumnas = 0;
+						int numFilas = 0;
+						DefaultListModel model_curso = new DefaultListModel();
+						List<CursoPropio> listacurso = new ArrayList<>();
+						listacurso = gestor.listarCursos();
 						String aux[] = new String[8];
 
 						if (chckbxNombre.isSelected()) {
-							aux[i] = chckbxNombre.getText();
-							i++;
-							
+							aux[numColumnas] = chckbxNombre.getText();
+							numColumnas++;
+
+							for (int j = 0; j < listacurso.size(); j++) {
+								model_curso.addElement(listacurso.get(numColumnas).getId());
+								numFilas++;
+							}
+
 						}
 						if (chckbxCentro.isSelected()) {
-							aux[i] = chckbxCentro.getText();
-							i++;
-							
+							aux[numColumnas] = chckbxCentro.getText();
+							numColumnas++;
+
 						}
 						if (chckbxCrditos.isSelected()) {
-							aux[i] = chckbxCrditos.getText();
-							i++;
-						
+							aux[numColumnas] = chckbxCrditos.getText();
+							numColumnas++;
+
 						}
 						if (chckbxImporte.isSelected()) {
-							aux[i] = chckbxImporte.getText();
-							i++;
-							
+							aux[numColumnas] = chckbxImporte.getText();
+							numColumnas++;
+
 						}
 						if (chckbxEdicin.isSelected()) {
-							aux[i] = chckbxEdicin.getText();
-							i++;
-							
+							aux[numColumnas] = chckbxEdicin.getText();
+							numColumnas++;
+
 						}
 						if (!campoFechaInicio.getText().equals("")) {
-							aux[i] = campoFechaInicio.getText();
-							i++;
-							
+							aux[numColumnas] = campoFechaInicio.getText();
+							numColumnas++;
+
 						}
 						if (!campoFechaFin.getText().equals("")) {
-							aux[i] = campoFechaFin.getText();
-							i++;
-							
-						}
-						if(!comboBox.getSelectedItem().toString().equals("Estado")) {
-							aux[i] = comboBox.getSelectedItem().toString();
-							i++;
-						}
-							
+							aux[numColumnas] = campoFechaFin.getText();
+							numColumnas++;
 
-						String campos[] = new String[i];
-						if(i==0) {
-							lblNingunCampoSelected.setText("SELECCIONE ALMENOS UN CAMPO");
+						}
+						if (!comboBox.getSelectedItem().toString().equals("Estado")) {
+							aux[numColumnas] = comboBox.getSelectedItem().toString();
+							numColumnas++;
 						}
 
-						 
+						Object matriz[][] = new Object[numFilas][numColumnas];
+
+						String campos[] = new String[numColumnas];
+						System.out.println(numColumnas);
+						if (numColumnas == 0) {
+							lblNewLabel.setText("SELECCIONE CAMPOS");
+						} else {
+
 							for (int k = 0; k < campos.length; k++) {
 								campos[k] = aux[k];
-								//System.out.println(campos[k]);
+								System.out.println(campos[k]);
 							}
-					
-						
-						realizarConsulta(campos);
+							
+							for (int i = 0; i < listacurso.size(); i++) {
+								matriz[i][0] = listacurso.get(i).getNombre();
+								System.out.println(matriz[i][0]);
+							}
+							
+							for (int j = 0; j < campos.length; j++) {
+								
+								if (campos[j].equals("Centro")) {
+									for (int i = 0; i < listacurso.size(); i++) {
+										matriz[i][j] = listacurso.get(i).getCentro();
+										System.out.println(matriz[i][j]);
+									}
+								}if (campos[j].equals("Créditos")) {
+									for (int i = 0; i < listacurso.size(); i++) {
+										matriz[i][j] = listacurso.get(i).getECTS();
+										System.out.println(matriz[i][j]);
+									}
+								}if (campos[j].equals("Importe")) {
+									for (int i = 0; i < listacurso.size(); i++) {
+										matriz[i][j] = listacurso.get(i).getTasaMatricula();
+										System.out.println(matriz[i][j]);
+									}
+								}if (campos[j].equals("Edición")) {
+									for (int i = 0; i < listacurso.size(); i++) {
+										matriz[i][j] = listacurso.get(i).getEdicion();
+										System.out.println(matriz[i][j]);
+									}
+								}if (campos[j].equals("Estado")) {
+									for (int i = 0; i < listacurso.size(); i++) {
+										matriz[i][j] = listacurso.get(i).getEstado();
+										System.out.println(matriz[i][j]);
+									}
+								}if (campos[j].equals("Validado")) {
+									
+								}if (campos[j].equals("")) {
+									
+								}if (campos[j].equals("")) {
+									
+								}if (campos[j].equals("")) {
+									
+								}if (campos[j].equals("")) {
+									
+								}
+								
+								
+								
+							}
+
+							rellenarMatriz(matriz);
+							realizarConsulta(matriz, campos);
+							setBounds(100, 100, 900, 400);
+
+						}
+
 					}
 				});
 			}
@@ -179,22 +249,27 @@ public class PantallaJefeGabineteVicerrectorado extends JFrame {
 
 	}
 
-	public void realizarConsulta(/*Object[][] matrizInfo,*/ String campos[]) {
+	public void realizarConsulta(Object[][] matrizInfo, String campos[]) {
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(159, 47, 515, 277);
+		scrollPane.setBounds(159, 47, 700, 277);
 		contentPane.add(scrollPane);
 
 		table = new JTable();
-		table.setModel(new DefaultTableModel(/*new object se remplazará por la matriz resultado de la consulta sql*/new Object[][] { { null, null, null, null, null, null, null, null },
-				{ null, null, null, null, null, null, null, null }, { null, null, null, null, null, null, null, null },
-				{ null, null, null, null, null, null, null, null }, { null, null, null, null, null, null, null, null },
-				{ null, null, null, null, null, null, null, null }, { null, null, null, null, null, null, null, null },
-				{ null, null, null, null, null, null, null, null }, { null, null, null, null, null, null, null, null },
-				{ null, null, null, null, null, null, null, null }, { null, null, null, null, null, null, null, null },
-				{ null, null, null, null, null, null, null, null }, { null, null, null, null, null, null, null, null },
-				{ null, null, null, null, null, null, null, null }, { null, null, null, null, null, null, null, null },
-				{ null, null, null, null, null, null, null, null }, }, campos));
+		table.setModel(new DefaultTableModel(matrizInfo, campos));
 		table.setToolTipText("");
 		scrollPane.setViewportView(table);
+	}
+
+	public Object[][] rellenarMatriz(Object[][] matriz) {
+
+		for (int columnas = 0; columnas < matriz.length; columnas++) {
+			for (int filas = 0; filas < matriz[columnas].length; filas++) {
+
+				System.out.println("c" + matriz[columnas][filas]);
+
+			}
+		}
+
+		return matriz;
 	}
 }
