b36b2115230fe14dd4a21f0d7519f98c025ba1bd
Daniel Durán Fernández <danielduranfernandez2002@gmail.com>
2022-12-07 11:51:53 +0100

    Configuración buildpath junit
    Configuración buildpath junit + dependencias pom

---
+++ src/test/java/negocio/controllers/GestorPropuestasCursosTest.java
@@ -0,0 +1,20 @@
+package negocio.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GestorPropuestasCursosTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void eliminarCurso() {
+//		assertTrue(comprobar curso eliminado);
+//		assertEquals(0, 0);
+	}
+
+}
